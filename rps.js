9f1265a4dd8ae107f8da3bf793a67a7678561c5c
Shehabmohamed86 <shehabmohamed8688@gmail.com>
2022-08-07 22:09:41 -0400

    game

--- rps.js
+++ rps.js
@@ -1,5 +1,19 @@
 
 function getComputerChoice(){
  let choise =Math.random();
-  return choise <= 0.34 ? 'rock' : choise  <= 0.67 ? 'paper' : choise = 'scissors';
+  return choise <= 0.34 ? 'rock' : choise  <= 0.67 ? 'paper' :  'scissors';
+  
+}
+
+
+
+function playRound(playerSelection, getComputerChoice){
+ 
+ if (playerSelection === 'rock' && getComputerChoice === 'paper') return 'you lose! paper beats rock';
+ else if (playerSelection === 'rock' && getComputerChoice === 'scissors') return 'you win, great work';
+ else if (playerSelection === 'paper' && getComputerChoice === 'rock') return 'you win, great work';
+ else if (playerSelection === 'paper' && getComputerChoice === 'scissors') return 'you lose! scissors beats paper';
+ else if (playerSelection === 'scissors' && getComputerChoice === 'paper') return 'you win, great work';
+ else if (playerSelection === 'scissors' && getComputerChoice === 'rock') return 'you lose! rock beats scissors';
+ else return "tie";
 }
